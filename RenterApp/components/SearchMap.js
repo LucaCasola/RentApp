2646e717f0f870ad77e4fefb176aad58ee8440af
kvora2 <kelvinvora21@gmail.com>
2024-04-11 17:18:50 -0400

    merge main to renderApp branch

--- RenterApp/components/SearchMap.js
+++ RenterApp/components/SearchMap.js
@@ -3,8 +3,7 @@ import { useState, useEffect, useRef } from "react";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
 
-export default function SearchMap({navigation}) {
-  const [permission, setPermission] = useState(false);
+export default function SearchMap({ navigation }) {
   const [location, setLocation] = useState(null);
   const [loading, setLoading] = useState(true);
   const mapViewRef = useRef(null);
@@ -21,11 +20,8 @@ export default function SearchMap({navigation}) {
 
       // get the current location if permission is granted
       if (permissionsObject.status === "granted") {
-        setPermission(true);
-        await getCurrentPosition();
-      }
-      else {
-        setPermission(false);
+        getCurrentPosition();
+      } else {
         alert("Permission denied or not provided");
         navigation.navigate("Account");
       }
@@ -36,29 +32,25 @@ export default function SearchMap({navigation}) {
 
   const getCurrentPosition = async () => {
     try {
-      if (permission) {
-        const currLocation = await Location.getCurrentPositionAsync({
-          accuracy: Location.Accuracy.Balanced,
-        });
+      const currLocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Balanced,
+      });
 
-        // set the location state to the current location
-        setLocation(currLocation);
-        setLoading(false);
+      // set the location state to the current location
+      setLocation(currLocation);
+      setLoading(false);
 
-        // logging user curr location info
-        console.log(currLocation);
+      // logging user curr location info
+      console.log(currLocation);
 
-        // animate the map to the current location
-        // maybe change to animateCamera later
-        mapViewRef?.current?.animateToRegion({
-          latitude: currLocation.coords.latitude,
-          longitude: currLocation.coords.longitude,
-          latitudeDelta: 0.1,
-          longitudeDelta: 0.1,
-        });
-      } else {
-        alert("Permission denied or not provided");
-      }
+      // animate the map to the current location
+      // maybe change to animateCamera later
+      mapViewRef?.current?.animateToRegion({
+        latitude: currLocation.coords.latitude,
+        longitude: currLocation.coords.longitude,
+        latitudeDelta: 0.1,
+        longitudeDelta: 0.1,
+      });
     } catch (err) {
       console.log(err);
     }
